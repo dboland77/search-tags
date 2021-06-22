6acdaaaa3327ecf3fada0950f2ae0780117909a9
Dave Boland <dboland77@gmail.com>
2021-06-22 11:31:05 +0100

    Grab data with axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+const URL = "https://my.api.mockaroo.com/movies.json?key=bf3c1c60";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+
+    const loadTags = async () => {
+      try {
+        const response = await axios.get(URL);
+        setData(response.data);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+        } else {
+          throw error;
+        }
+      }
+    };
+
+    loadTags();
+  
+    return () => {
+      source.cancel();
+    };
+  }, []);
+
+  console.log(data);
+  
+  return <div className="App">
+    <p></p>
+  </div>;
 }
 
 export default App;
